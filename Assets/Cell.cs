1f481a9c18a8fd6f3ed95199e306d3f996cb0c2b
DenialHandrails <danielhonrales@gmail.com>
2020-10-27 00:21:22 -0500

    Made map grid
    it was pain

---
+++ Assets/Cell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+
+    public int size = 1;
+    bool closed = true;
+
+    public Cell(){
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Maze.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Maze : MonoBehaviour
+{
+
+    (int width, int height) size = (5, 5);
+    (int x, int y) origin = (0, 0);
+    GameObject[,] mazescape;
+    
+    public GameObject cellPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Generate new closed maze
+        mazescape = new GameObject[size.height, size.width];
+
+        for(int i = 0; i < mazescape.GetLength(0); i++){
+            for(int j = 0; j < mazescape.GetLength(1); j++){
+                Debug.Log(new Vector3(origin.x + (j * cellPrefab.GetComponent<Cell>().size), origin.y + (i * cellPrefab.GetComponent<Cell>().size), 0));
+                mazescape[i,j] = Instantiate(cellPrefab, new Vector3(origin.x + (j * cellPrefab.GetComponent<Cell>().size), origin.y + (i * cellPrefab.GetComponent<Cell>().size), 0), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
