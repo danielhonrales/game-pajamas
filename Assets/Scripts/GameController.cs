b1f8f03ab232eb932001d3398004faa4eec63410
DenialHandrails <danielhonrales@gmail.com>
2020-10-30 23:18:51 -0500

    START MENU

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
 
+    public bool gameActive = false;
     public GameObject escapePrompt;
 
     public int generators = 0;
---
+++ Assets/StartButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+
+    public GameObject panel;
+    public GameObject gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void bye(){
+        gameController.GetComponent<GameController>().gameActive = true;
+        panel.SetActive(false);
+    }
+}
