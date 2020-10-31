e5daa2af7ae05db2a72cdb9ab39bc9cfecd5a8f2
wasif1771 <wasif.r1771@gmail.com>
2020-10-30 23:01:07 -0500

    gabbere

---
+++ Assets/Scripts/JumpScare.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+ 
+public class JumpScare : MonoBehaviour {
+ 
+    public Image image;
+    public AudioSource audioSource;
+ 
+    void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(JumpScare_CR());
+    }
+ 
+    IEnumerator JumpScare_CR()
+    {
+        image.enabled = true;
+        audioSource.Play();
+        yield return new WaitForSeconds(0.5f);
+        image.enabled = false;
+    }
+}
