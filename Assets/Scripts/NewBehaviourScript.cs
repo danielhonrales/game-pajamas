242238387251e5ded3990167a5545d2f4cc2d982
Wintrxu <sportyfan312@gmail.com>
2020-10-30 20:13:59 -0500

    Generator

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -2,103 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GeneratorTask : MonoBehaviour
 {
-    
-    using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace RandomNumberGame
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Random gen = new Random();
-            int number;
-            bool over = false;
-            string userChoice;
-            int guess = 0;
-            int counter = 0;
+    public bool generatorOn;
+    public GameObject player;
+    public int numberGeneratorsMax;
 
-            while (!over)
-            {
-                over = true;
-                Console.WriteLine("1 - Easy: 1 - 10");
-                Console.WriteLine("2 - Medium: 1 - 50");
-                Console.WriteLine("3 - Hard: 1 - 100");
-                Console.Write("Please input the number of the difficulty you wish to  play: ");
-                string str = Console.ReadLine();
 
-                switch (str)
-                {
-                    case "1":
-                        Console.WriteLine("You chose easy difficulty.");
-                        number = gen.Next(1, 11);
-
-                        Console.WriteLine("I'm thinking of a number between 1 - 10");
-                        for (guess = Convert.ToInt32(Console.ReadLine()); guess != number; guess = Convert.ToInt32(Console.ReadLine()))
-                        {
-                            if (guess < 1 || guess > 10)
-                            {
-                                Console.WriteLine("Please enter a number between 1 - 10");
-                            }
-                            else if (guess < number)
-                            {
-                                Console.WriteLine("Higher");
-                                counter++;
-                                Console.WriteLine("You have " + (5 - counter) + " attempts left");
-                            }
-                            else if (guess > number)
-                            {
-                                Console.WriteLine("Lower");
-                                counter++;
-                                Console.WriteLine("You have " + (5 - counter) + " attempts left");
-                            }
-                            else
-                            {
-                                Console.WriteLine("You have guessed correctly! It only took you: " + number + " tries.");
-                                Console.WriteLine("Would you like to play again? (y/n)");
-                                userChoice = Console.ReadLine();
-                                if (userChoice == "n")
-                                {
-                                    Console.WriteLine("Thanks for playing!");
-                                    Environment.Exit(0);
-                                }
-                                else if (userChoice == "y")
-                                {
-                                    over = false;
-                                }
-                            }
-                            if (counter == 5)
-                            {
-                                Console.WriteLine("Sorry, you didn't guess it under 5 attempts.");
-                                Console.WriteLine("Would you like to play again? (y/n)");
-                                userChoice = Console.ReadLine();
-                                if (userChoice == "n")
-                                {
-                                    Console.WriteLine("Thanks for playing!");
-                                    Environment.Exit(0);
-                                }
-                                else if (userChoice == "y")
-                                {
-                                    over = false;
-                                }
-                            }
-                        }
-                        break;
     // Start is called before the first frame update
     void Start()
     {
-        
+        generatorOn = false;
+        numberGeneratorsMax = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    bool GeneratorCollison()
+    {
+        for (Genrator generator in generators)
+        {
+            if (player.position.x + player.size.width > generator.position.x &&
+            player.position.x < generator.position.x + generator.size.width &&
+            player.position.y + player.size.height > generator.position.y &&
+            player.position.y < generator.position.y + generator.size.height)
+            {
+                //Debug.Log("Collision!");
+                return true;
+            }
+        }
+        //Debug.Log("Not in collision...")
+        return false;
+    }
+
+    bool GeneratorTask(generatorOn)
+    {
+        if (GeneratorCollison() == true)
+        {
+            generatorOn == true;
+        }
+        else
+        {
+            generatorOn == false;
+        }
+
+        return false;
     }
 }
